91882fb0dd34436d9392abc8577be7b2258e6f83
NO ONE <sohaibikram249@gmail.com>
2024-08-17 16:36:57 +0500

    update

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Skill from "./Components/Skills/skill";
 import Education from "./Components/Education/edu";
 import Project from "./Components/Projects/projects";
 import Contact from "./Components/Contact/contact";
+import Footer from "./Components/Footer/footer";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Skill />
       <Project />
       <Contact />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./footer.css"; 
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="social">
+        <a href="https://www.linkedin.com/in/sohaib-ikram249/">
+          <i className="bx bxl-linkedin"></i>
+        </a>
+        <a href="https://github.com/callmesohaib">
+          <i className="bx bxl-github"></i>
+        </a>
+        <a href="https://www.instagram.com/ufff_yawrrr_/?next=%2F">
+          <i className="bx bxl-instagram"></i>
+        </a>
+        <a href="https://www.facebook.com/sohaib.ikram.9461">
+          <i className="bx bxl-facebook-circle"></i>
+        </a>
+      </div>
+      <ul className="list">
+        <li>
+          <a href="#project">Projects</a>
+        </li>
+        <li>
+          <a href="#home">About Me</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <p className="copyright">
+        &copy; Sohaib Ikram | All Rights Reserved
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
