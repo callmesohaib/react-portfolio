7f6f5dbad18f50528bdb98ce4ecf3fc347aa71e1
NO ONE <sohaibikram249@gmail.com>
2024-08-18 09:29:28 +0500

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ToastContainer, Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import App from "./App";
+import './toastify.css'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
