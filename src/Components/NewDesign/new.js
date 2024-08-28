96549101e7fe94434c2e824ce8d634674b48c08e
NO ONE <sohaibikram249@gmail.com>
2024-08-28 09:27:23 +0500

    update

--- src/Components/NewDesign/new.js
+++ src/Components/NewDesign/new.js
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./new.css";
 import projectData from "../../Json/project.json";
-import { Link } from "react-scroll";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
 
 const New = () => {
+  const [menuItems, SetMenuItem] = useState(projectData);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      SetMenuItem(projectData); 
+    } else {
+      const updateList = projectData.filter((curElement) => {
+        return curElement.category === category;
+      });
+      SetMenuItem(updateList);
+    }
+  };
+
   return (
     <section className="pro" id="pro" style={{ color: "white" }}>
       <h2 className="heading" style={{ color: "white" }}>
-        <i class="fa-solid fa-laptop-code laptop"></i>
+        <i className="fa-solid fa-laptop-code laptop"></i>
         Projects
       </h2>
       <div className="projectNavbar">
-        <Link className="projectMenuList">All</Link>
-        <Link className="projectMenuList">Frontend</Link>
-        <Link className="projectMenuList">Backend</Link>
-        <Link className="projectMenuList">Python</Link>
+        <button className="projectMenuList" onClick={() => filterItem("All")}>
+          All
+        </button>
+        <button className="projectMenuList" onClick={() => filterItem("Frontend")}>
+          Frontend
+        </button>
+        <button className="projectMenuList" onClick={() => filterItem("Backend")}>
+          Backend
+        </button>
+        <button className="projectMenuList" onClick={() => filterItem("Python")}>
+          Python
+        </button>
       </div>
       <div className="projects">
-        {projectData.map((project, index) => (
+        {menuItems.map((project, index) => (
           <div className="project" key={index}>
             <img
               src={require(`../../ProjectImages/${project.img}`)}
@@ -31,7 +53,7 @@ const New = () => {
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                <i class="bx bxl-github git"></i>
+                <i className="bx bxl-github git"></i>
               </a>
             </div>
           </div>
