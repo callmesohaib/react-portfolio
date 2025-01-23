653d9a91c8129f57c7094cf428b09c5df7ea6ae6
Sohaib Ikram <2022CS621@student.uet.edu.pk>
2025-01-23 21:14:21 +0500

    update

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -12,7 +12,7 @@ const Intro = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsDesktop(window.innerWidth > 480); // Set desktop condition
+      setIsDesktop(window.innerWidth > 480); 
     };
 
     window.addEventListener("resize", handleResize);
@@ -21,86 +21,92 @@ const Intro = () => {
 
   return (
     <section id="intro">
+    <motion.div
+      variants={fadeIn("up", 0.2)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.7 }}
+      className="introContent"
+    >
+      {/* Conditionally render the circular image for mobile */}
+      {!isDesktop && (
+        <div className="imageContainer">
+          <img src={bg} alt="Profile" className="mobileBg" />
+        </div>
+      )}
+  
+      <span className="hello">Hello,</span>
+      <span className="introText">
+        I'm <span className="introName">Sohaib </span>Ikram
+        <br />
+        <span className="introJob">
+          <TypeAnimation
+            sequence={[
+              "Mern Stack Developer",
+              1000,
+              "UI/UX Designer",
+              1000,
+              "Programmer",
+              1000,
+              "Code Analyst",
+              1000,
+            ]}
+            wrapper="span"
+            speed={50}
+            style={{ fontSize: "2rem", display: "inline-block" }}
+            repeat={Infinity}
+          />
+        </span>
+      </span>
+      <p className="introPara">
+        I'm a passionate and dedicated Mern Stack Web Developer with a knack for
+        solving complex problems and a love for crafting elegant and efficient web
+        solutions.
+      </p>
       <motion.div
-        variants={fadeIn("up", 0.2)}
+        variants={fadeIn("left", 0.2)}
         initial="hidden"
         whileInView={"show"}
+        style={{ opacity: 0 }}
         viewport={{ once: false, amount: 0.7 }}
-        className="introContent"
+        className="social"
       >
-        <span className="hello">Hello,</span>
-        <span className="introText">
-          I'm <span className="introName">Sohaib </span>Ikram
-          <br />
-          <span className="introJob">
-            <TypeAnimation
-              sequence={[
-                "Mern Stack Developer",
-                1000,
-                "UI/UX Designer",
-                1000,
-                "Programmer",
-                1000,
-                "Code Analyst",
-                1000,
-              ]}
-              wrapper="span"
-              speed={50}
-              style={{ fontSize: "2rem", display: "inline-block" }}
-              repeat={Infinity}
-            />
-          </span>
-        </span>
-        <p className="introPara">
-          I'm a passionate and dedicated Mern Stack Web Developer with a knack
-          for solving complex problems and a love for crafting elegant and
-          efficient web solutions.
-        </p>
-        <motion.div
-          variants={fadeIn("left", 0.2)}
-          initial="hidden"
-          whileInView={"show"}
-          style={{ opacity: 0 }}
-          viewport={{ once: false, amount: 0.7 }}
-          className="social"
+        <a target="_blank" href="https://www.linkedin.com/in/sohaib-ikram249/">
+          <i className="bx bxl-linkedin"></i>
+        </a>
+        <a target="_blank" href="https://github.com/callmesohaib">
+          <i className="bx bxl-github"></i>
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/sohaib.__.hoon/?next=%2F"
         >
-          <a target="_blank" href="https://www.linkedin.com/in/sohaib-ikram249/">
-            <i className="bx bxl-linkedin"></i>
-          </a>
-          <a target="_blank" href="https://github.com/callmesohaib">
-            <i className="bx bxl-github"></i>
-          </a>
-          <a target="_blank" href="https://www.instagram.com/sohaib.__.hoon/?next=%2F">
-            <i className="bx bxl-instagram"></i>
-          </a>
-          <a target="_blank" href="https://www.facebook.com/sohaib.ikram.9461">
-            <i className="bx bxl-facebook-circle"></i>
-          </a>
-        </motion.div>
-
-        <div className="buttons">
-          <a href={CV} download="Sohaib-CV.pdf" className=" Resume btn">
-            Resume
-            <i className="bx bxs-note cv"></i>
-          </a>
-
-          <Link
-            to="contact"
-            spy={true}
-            smooth={true}
-            duration={500}
-            offset={-80}
-          >
-            <button className="btn">
-              Hire Me
-              <i className="bx bx-right-arrow-alt"></i>
-            </button>
-          </Link>
-        </div>
+          <i className="bx bxl-instagram"></i>
+        </a>
+        <a target="_blank" href="https://www.facebook.com/sohaib.ikram.9461">
+          <i className="bx bxl-facebook-circle"></i>
+        </a>
       </motion.div>
-      {/* Conditional rendering for background image */}
-      {isDesktop && <img src={bg} alt="Background" className="bg" />}
-    </section>
+  
+      <div className="buttons">
+        <a href={CV} download="Sohaib-CV.pdf" className=" Resume btn">
+          Resume
+          <i className="bx bxs-note cv"></i>
+        </a>
+  
+        <Link to="contact" spy={true} smooth={true} duration={500} offset={-80}>
+          <button className="btn">
+            Hire Me
+            <i className="bx bx-right-arrow-alt"></i>
+          </button>
+        </Link>
+      </div>
+    </motion.div>
+  
+    {/* Show the full image only for desktop */}
+    {isDesktop && <img src={bg} alt="Background" className="bg" />}
+  </section>
+  
   );
 };
 
